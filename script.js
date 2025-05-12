05ffab8ad17cb60c808506089b394f66fd93251e
Bhanuprakash6923 <158007213+Bhanuprakash6923@users.noreply.github.com>
2025-05-12 10:42:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 function convertToRoman(num) {
-  if (num <= 0 || num > 100000) return 'Invalid input';
+    const obj = {
+        0: ['M', 1000], 
+        1: ['D', 500], 
+        2: ['C', 100], 
+        3: ['L', 50], 
+        4: ['X', 10], 
+        5: ['V', 5], 
+        6: ['I', 1]
+    };
 
-  const romanNumerals = [
-    ['M', 1000],
-    ['CM', 900],
-    ['D', 500],
-    ['CD', 400],
-    ['C', 100],
-    ['XC', 90],
-    ['L', 50],
-    ['XL', 40],
-    ['X', 10],
-    ['IX', 9],
-    ['V', 5],
-    ['IV', 4],
-    ['I', 1]
-  ];
+    let romanNumeral = '';
+    
+    // Array of values and symbols in descending order
+    const values = [
+        ['M', 1000], 
+        ['CM', 900], 
+        ['D', 500], 
+        ['CD', 400], 
+        ['C', 100], 
+        ['XC', 90], 
+        ['L', 50], 
+        ['XL', 40], 
+        ['X', 10], 
+        ['IX', 9], 
+        ['V', 5], 
+        ['IV', 4], 
+        ['I', 1]
+    ];
 
-  let result = '';
-
-  for (const [symbol, value] of romanNumerals) {
-    while (num >= value) {
-      result += symbol;
-      num -= value;
+    for (let i = 0; i < values.length; i++) {
+        while (num >= values[i][1]) {
+            romanNumeral += values[i][0];
+            num -= values[i][1];
+        }
     }
-  }
 
-  return result;
+    return romanNumeral;
 }
 
-// Example usage:
-console.log(convertToRoman(14));     // Output: 'XIV'
-console.log(convertToRoman(798));    // Output: 'DCCXCVIII'
-console.log(convertToRoman(3999));   // Output: 'MMMCMXCIX'
-console.log(convertToRoman(10000));  // Output: 'MMMMMMMMMM'
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+
+// console.log(convertToRoman(36));
+
+// do not edit below this line
+module.exports = convertToRoman;
